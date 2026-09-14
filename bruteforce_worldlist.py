ccbad6268b246d7e6fe25707dedf0bd704f03ef1
Elias Myarkovskiy <elaimyar@gmail.com>
2026-09-14 21:57:50 +0100

    Implement AES encryption in CBC mode
    Added support for AES encryption in CBC mode with an IV.

--- bruteforce_worldlist.py
+++ bruteforce_worldlist.py
@@ -17,17 +17,23 @@ known_plain_block = plaintext[:32]
 # Define a wordlist or a logical range of keys to test
 passlist = [key for key in passlist if len(key) <= 16]
 
+iv = b'\x00' * 16
+
 # print(passlist)
 for password in passlist:
     # Keys must be 16, 24, or 32 bytes. Pad your guess with zeros or encode it:
     key = password.encode('utf-8').ljust(16, b'\x00') 
 
-    # Try to encrypt the known plaintext with this guess
-    cipher = AES.new(key, AES.MODE_c)
+    # Uncomment to encrypt with AES_ECB mode
+    # cipher = AES.new(key, AES.MODE_ECB)
+
+    # Uncomment to encrypt with AES_CBC mode
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    
     test_ciphertext = cipher.encrypt(known_plain_block)
     
     # print(test_ciphertext.decode('latin-1', errors='ignore'))
-    # If it matches, you found the key!
+    # If match = key found
     if test_ciphertext == target_block:
         print(f"Success! The key is: {password}")
         break
